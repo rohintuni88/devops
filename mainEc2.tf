c56860c02739b9fe3538396dd471ff3aba51df0a
RohinKL <rohintuni88@gmail.com>
2024-01-04 21:08:05 +0800

    updated code, security group added

--- mainEc2.tf
+++ mainEc2.tf
@@ -1,8 +1,36 @@
 resource "aws_instance" "web" {
   ami           = "ami-03265a0778a880afb"
   instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.mySecurity4Jan.id] #this means list
 
   tags = {
     Name = "HelloRohin"
   }
+}
+
+resource "aws_security_group" "mySecurity4Jan" { #terraform name
+    name = "mySecurity4Jan_aws" # aws name
+    description = "allow all inbound traffic"
+    #vpc_id = aws_vpc.main.id 
+
+    ingress {
+        description = "Allow all port"
+        from_port = 0
+        to_port = 0
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+        #ipv6_cidr_blocks = 
+
+    }
+    
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    tags ={
+        Name ="HelloRohin"
+    }
 }
