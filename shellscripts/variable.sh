b1021cc346d4bb93a48289e7d4c8a14f3391ed21
rohintuni88@gmail.com <rohintuni88@gmail.com>
2024-01-21 14:46:36 +0800

    shell variables

---
+++ shellscripts/variable.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# --> # shebang --> to check the syntax of our shellscript, we need to give the location of bash
+
+PERSON1="Hi $PERSON2"
+PERSON2="Hello $PERSON1"
+
+
+echo "$PERSON1$PERSON2"   # to print
