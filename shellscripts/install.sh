71f3e70b84ac8e65ca68d248664d3512735cd0a1
rohintuni88@gmail.com <rohintuni88@gmail.com>
2024-01-21 18:24:57 +0800

    no message

--- shellscripts/install.sh
+++ shellscripts/install.sh
@@ -3,15 +3,26 @@
 uid=$(id -u) 
 
 if [ $uid -ne 0 ]
+then
    echo "Not Root"
    exit 1
-else
-   echo "you are in Root"
+ else
+   echo "You are in Root" 
 fi
+
 yum install git -y
 if [ $? -ne 0 ]
 then
-   echo "Error Installation Failed $?"
+   echo "Error $?"
+   exit 1
+else 
+   echo "Git Installation sucessful"
+fi
+
+yum install mysql -y
+if [ $? -ne 0 ]
+then
+   echo "Error $?"
    exit 1
 else 
    echo "Git Installation sucessful"
