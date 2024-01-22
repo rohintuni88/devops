32a264dd66a2031904e3a2dfe82b47fecac86172
rohintuni88@gmail.com <rohintuni88@gmail.com>
2024-01-22 09:12:11 +0800

    Any package also will install

--- shellscripts/loops.sh
+++ shellscripts/loops.sh
@@ -5,3 +5,37 @@ do
   echo "$i"
 done 
 
+#Install packages using loops like git, mysql, gcc, postfix, nettools
+
+USERVALIDATION(){
+uid=$(id -u) 
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+if [ $uid -ne 0 ]
+then
+   echo "Not Root"
+   exit 1
+ else
+   echo "You are in Root" 
+fi
+}
+
+INSATALLAPPS(){
+  for package in $@
+  do
+     yum list installed $package &>> $LOGFILE  # check installed or not
+     if[ $? -ne 0 ]
+     then
+        yum install $package -y &>>  $LOGFILE  # Install packages
+     else
+        echo "$package is already installed
+      fi
+  done
+}
+
+USERVALIDATION &>> $LOGFILE
+echo "All Args passed: $@"
+
+ 
+
